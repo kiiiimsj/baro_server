c6c7b054993eb33013faeeb40aa5f8434005ced9
leeseungjae97 <sky_battle@naver.com>
2020-10-28 13:03:23 +0900

    11

--- wantchu_server2/src/main/java/com/wantchu/wantchu_server2/config/ControllerConfigure.java
+++ wantchu_server2/src/main/java/com/wantchu/wantchu_server2/config/ControllerConfigure.java
@@ -82,7 +82,7 @@ public class ControllerConfigure {
     public DataSource dataSource() {
         DataSource dataSource = new DataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://wantchu-server.cszxzzjyxxdv.ap-northeast-2.rds.amazonaws.com:3306/wantchu?autoReconnection=true");
+        dataSource.setUrl("jdbc:mysql://baro.c3ptb78nhjkk.ap-northeast-2.rds.amazonaws.com:3306/baro?autoReconnection=true");
         dataSource.setUsername("root");
         dataSource.setPassword("qwerasdf12");
         dataSource.setInitialSize(2);
