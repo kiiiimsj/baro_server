087f095e41b4dde885b5529aa4c4384219406a94
kiiiimsj <kiiiimsj@naver.com>
2020-12-11 21:06:58 +0900

    123

--- wantchu_server2/src/main/java/com/wantchu/wantchu_server2/vo/OrderListVo.java
+++ wantchu_server2/src/main/java/com/wantchu/wantchu_server2/vo/OrderListVo.java
@@ -44,4 +44,19 @@ public class OrderListVo {
         map.put("store_phone", store_phone);
         return map;
     }
+
+    public HashMap<String, Object> convertMapOnlyHour() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("receipt_id", receipt_id);
+        map.put("store_name", store_name);
+        map.put("order_date", DateConverter.convertDateOnlyHour(order_date));
+        map.put("total_count", total_count);
+        map.put("order_state",order_state);
+        map.put("store_image", store_image);
+        map.put("store_latitude", store_latitude);
+        map.put("store_longitude", store_longitude);
+        map.put("store_id", store_id);
+        map.put("store_phone", store_phone);
+        return map;
+    }
 }
