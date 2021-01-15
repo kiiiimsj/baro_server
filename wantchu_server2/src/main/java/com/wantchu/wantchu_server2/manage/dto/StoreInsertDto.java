800443ad97edd673433942aa5d0e2aa33e23820e
hty15973 <61073154+hty15973@users.noreply.github.com>
2021-01-15 14:42:02 +0900

    dasdf

--- wantchu_server2/src/main/java/com/wantchu/wantchu_server2/manage/dto/StoreInsertDto.java
+++ wantchu_server2/src/main/java/com/wantchu/wantchu_server2/manage/dto/StoreInsertDto.java
@@ -20,8 +20,10 @@ public class StoreInsertDto {
     private String store_info;
     private String store_image;
     private String owner_id;
+    private String representative_name;
+    private String business_number;
 
-    public StoreInsertDto(String type_code, String store_name, Double store_latitude, Double store_longitude, String store_opentime, String store_closetime, String store_phone, String store_dayoff, String store_location, String store_info, String store_image,String owner_id) {
+    public StoreInsertDto(String type_code, String store_name, Double store_latitude, Double store_longitude, String store_opentime, String store_closetime, String store_phone, String store_dayoff, String store_location, String store_info, String store_image, String owner_id, String representative_name, String business_number) {
         this.type_code = type_code;
         this.store_name = store_name;
         this.store_latitude = store_latitude;
@@ -34,5 +36,7 @@ public class StoreInsertDto {
         this.store_info = store_info;
         this.store_image = store_image;
         this.owner_id = owner_id;
+        this.representative_name = representative_name;
+        this.business_number = business_number;
     }
 }
