1e19e57e2b3e0e7791a060c4420a864c9a90d359
hty15973 <61073154+hty15973@users.noreply.github.com>
2021-03-20 15:55:00 +0900

    afds

--- wantchu_server2/src/main/java/com/wantchu/wantchu_server2/coupon/exception/CouponNotInsertException.java
+++ wantchu_server2/src/main/java/com/wantchu/wantchu_server2/coupon/exception/CouponNotInsertException.java
@@ -3,7 +3,7 @@ package com.wantchu.wantchu_server2.coupon.exception;
 public class CouponNotInsertException extends CouponException {
     private static final long serialVersionUID = 1L;
     public static final int ERRNO = 4;
-    public static final String MESSAGE = "이미 사용한 쿠폰이거나 쿠폰번호가 알맞지 않습니다.";
+    public static final String MESSAGE = "이미 사용한 쿠폰이거나 쿠폰번호가 유효하지 않습니다.";
 
     public CouponNotInsertException() {
         super(ERRNO, MESSAGE);
